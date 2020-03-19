bceeb69f2776ce4713b3181f2da5ffa4e187040e
pzubar <pmzubar@gmail.com>
2020-03-20 00:00:01 +0200

    Adds jwt support to auth service

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {
   }
 
   async validateUser(username: string, pass: string): Promise<any> {
@@ -13,5 +14,12 @@ export class AuthService {
       return result
     }
     return null
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId }
+    return {
+      access_token: this.jwtService.sign(payload)
+    }
   }
 }
