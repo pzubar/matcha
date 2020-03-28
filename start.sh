06e9189ddc7ab2e5ac25c37ff304333047be8968
mbiliaie <mbiliaie@student.unit.ua>
2020-03-29 00:12:32 +0200

    added simultaneous start of both servers

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
-yarn install && yarn run start:dev
 #!/bin/bash
 ROOTDIR=./php
 PORT=8100
 
+source ./sendgrid.env
 cd $ROOTDIR || exit
-source ./third-party/sendgrid.env
 
 #On iOS: this way built-in server supports ipv6 only
-php -S localhost:$PORT -c cli_server.ini
-
-#On iOS: server supports ipv4 only
-#php -S 127.0.0.1:$PORT -c cli_server.ini
+(php -S localhost:$PORT -c cli_server.ini) & (yarn install && yarn run start:dev)
