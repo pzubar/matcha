8b7fca5066b8e03a063d14e1eb7fd8ac29914585
pzubar <pmzubar@gmail.com>
2020-04-10 23:36:29 +0300

    Improves server endpoints, removes /api

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,25 @@ async function bootstrap() {
 
   app.use(requestIp.mw())
   console.log(`Server runs on http://localhost:${port}/`)
+  /**
+   * async function bootstrap() {
+    const server = express();
+    const config = require('../../etc/config.js');
+
+    const apiFactory = new NestFactoryStatic();
+    const api = await apiFactory.create(ApiModule, server);
+    api.setGlobalPrefix("/api/v1");
+    await api.init();
+
+    const adminFactory = new NestFactoryStatic();
+    const admin = await adminFactory.create(AdminModule, server);
+    admin.setGlobalPrefix("/admin");
+    await admin.init();
+
+    http.createServer(server).listen(config.server.port);
+}
+
+   bootstrap();
+   */
 }
 bootstrap()
