9badfcad38d0c22e9d9bea94cb749b784f1a8a33
Maksym Biliaievskyi <36799049+mbiliaie@users.noreply.github.com>
2020-04-16 20:09:02 +0300

    Adds mail sending API

---
+++ php/mail/query.php
@@ -0,0 +1,85 @@
+<?php
+include "./utilities.php";
+include './templates.php';
+
+/*
+ * POST requests are used for sending emails &
+ * GET requests are used for a forgotten password recovery
+ * or for a new account confirmation
+ */
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $postData = file_get_contents('php://input');
+    $data = json_decode($postData, true);
+
+    $userid = ($data["userid"]);
+    $event = ($data["event"]);
+    $data = ($data["data"]);
+
+    if (empty($userid)) {
+        error(5.00, 'no user id');
+        exit();
+    } else {
+        $userData = getUserData($userid);
+    }
+
+    function mailDispatcher($userid, $event, $userData) {
+        switch ($event) {
+            case "like":
+                $msgDetails = likeTemplate($userData["login"]);
+                break;
+            case "check":
+                $msgDetails = checkTemplate($userData["login"]);
+                break;
+            case "message":
+                $msgDetails = newMessageTemplate($userData["login"]);
+                break;
+            case "mutual":
+                $msgDetails = mutualTemplate($userData["login"]);
+                break;
+            case "dislike":
+                $msgDetails = dislikeTemplate($userData["login"]);
+                break;
+            case "forgotten":
+                $msgDetails = sendPasswordTemplate($userid, $userData);
+                break;
+            case "signup":
+                $msgDetails = signupTemplate($userid, $userData);
+                break;
+            default:
+                error(5.02, 'no event in request');
+                $msgDetails = false;
+        }
+        return $msgDetails;  //$msgDetails = [ "title" => "Some title", "msg" => "Some text"];
+    }
+
+    $content = mailDispatcher($userid, $event, $userData);
+    if ($content != false) {
+        include "./mailsender.php";
+        mailSend($userData["address"], $content);
+    }
+} // A decryption starts here by GET request:
+elseif ($_SERVER["REQUEST_METHOD"] == "GET"){
+    if ( !empty($_REQUEST ["verify"])){
+        $target = ($_GET["verify"]);
+        $event = "Signup";
+    } elseif ( !empty($_REQUEST ["recover"])) {
+        $target = ($_GET["recover"]);
+        $event = "Forgotten";
+    } else {
+        header("HTTP/1.1 422 Unprocessable Entity."); // Send when a request contains no expected input
+    }
+
+    $idTimeLock = getIdTimeLock($target);
+    $expirationPeriod  = 600; // 10 minutes. Here it is possible to choose any 'life time' for links
+
+    if ((time() - $idTimeLock["sent"]) <= $expirationPeriod) {
+        $userData = getUserData($idTimeLock["userid"]);
+        handleLink($event, $idTimeLock["userid"], $userData, $idTimeLock);
+    } else {
+        header("HTTP/1.1 403 Forbidden."); // This link has expired, user needs to request a new link
+    }
+} else {
+    exit();
+}
---
+++ php/mail/utilities.php
@@ -0,0 +1,58 @@
+<?php
+function error($code, $txt) {
+    echo "Critical error " . $code . " - " . $txt;
+    return $code;
+}
+
+function verifyNewAccount($id) {
+    try {
+        $sqlQuery = "UPDATE users SET is_verified = true WHERE id = :id AND is_verified = false";
+        $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=matcha_db;user=postgres;password=');
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $qres = $dbh->prepare($sqlQuery);
+        $qres->bindParam(':id',$id);
+        $qres->execute();
+        //$qres = $qres->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $msg) {
+        error(5.03, $msg->getMessage());
+        die();
+    }
+}
+
+function getUserData($id) {
+    try {
+        $sqlQuery = "SELECT * FROM users WHERE id = :id";
+        $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=matcha_db;user=postgres;password=');
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $qres = $dbh->prepare($sqlQuery);
+        $qres->bindParam(':id',$id);
+        $qres->execute();
+        $qres = $qres->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $msg) {
+        error(5.01, $msg->getMessage());
+        die();
+    }
+    return [
+        "address" => $qres[0]["email"],
+        "login" => $qres[0]["username"],
+        "created" => $qres[0]["created_at"]
+    ];
+}
+
+function getIdTimeLock($str) {
+    $firstPcnt = strpos($str, "%");
+
+    $userid = base64_decode(substr($str, 0, $firstPcnt));
+
+    $str = rawurldecode(substr($str, $firstPcnt + 1));
+    $lastPcnt = strrpos($str, "%");
+    $sent = base64_decode(substr($str, $lastPcnt));
+    $lock = substr($str, 0, $lastPcnt);
+
+    return [
+        "userid" => $userid,
+        "lock" => $lock,
+        "sent" => $sent,
+    ];
+}
+
