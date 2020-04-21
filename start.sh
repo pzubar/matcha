03e8b2c0b49212e90097f3f38812078d388af71f
Max <Max@localhost.localdomain>
2020-04-21 16:33:20 +0300

    changed localhost to ip adr

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 ROOTDIR=./php
 PORT=8100
+
 source ./sendgrid.env
 source  ./.env
 cd $ROOTDIR || exit
 
-#On iOS: this way built-in server supports ipv6 only
-# & (yarn install && yarn run start:dev)
-
 trap 'kill %1;' SIGINT
-(php -S localhost:$PORT -c cli_server.ini) | tee php.log | sed -e 's/^/[PHP Server] /' &
+(php -S 127.0.0.1:$PORT -c cli_server.ini) | tee php.log | sed -e 's/^/[PHP Server] /' &
 (yarn install && yarn run migrate up && yarn run start:dev) | tee node.log | sed -e 's/^/[NodeJS Server] /'
